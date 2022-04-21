47757f88ef1411f63036144b23a5875678e0850c
masasixpack <81848199+masasixpack@users.noreply.github.com>
2022-04-22 00:03:14 +0900

    mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -159,3 +159,38 @@
 // console.log(arr8);
 // console.log(arr4);
 // // 元の配列に影響が出てしまうため、このようなコピーはしない
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+// const nameArr = ["田中", "山田", "ネロ"];
+// // 従来
+// // for (let index = 0; index < nameArr.length; index++) {
+// //   // console.log(nameArr[index]);
+// //   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// // }
+
+// // // map
+// // const nameArr2 = nameArr.map((name)=>{
+// //   return name;
+// // })
+// // console.log(nameArr2);
+
+// // nameArr.map((name) => console.log(name));
+// // nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// // //filter ある条件に一致したものだけ返却して新しい配列を生成する
+// // const numArr = [1, 2, 3, 4, 5];
+// // const newNumArr = numArr.filter((num) => {
+// //   return num % 2 === 1;
+// // });
+// // console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "ネロ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
