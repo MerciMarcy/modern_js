795fe9f9e6c80a581bfe4a8a266223610548e665
masasixpack <81848199+masasixpack@users.noreply.github.com>
2022-04-22 00:18:46 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -194,3 +194,21 @@
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時　: 条件がfolseの時
+// const vall = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(vall);
+
+// const num = 1300;
+// // console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています！！'　: '許容範囲内です';
+// };
+// console.log(checkSum(50, 60));
