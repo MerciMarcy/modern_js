dc91eed2baab388076ccb55010dd864b51d700fc
masasixpack <81848199+masasixpack@users.noreply.github.com>
2022-04-22 00:46:22 +0900

    セクションまとめ

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@
  * const,let等の変数宣言
  */
 //  // 従来のJavaScript
-//  var val1 = "var変数";
-//  console.log(val1);
+var val1 = "var変数";
+console.log(val1);
 
 //  // var変数は上書き可能
-//  val1 = "var変数を上書き";
-//  console.log(val1);
+val1 = "var変数を上書き";
+console.log(val1);
 
 //  // var変数は再宣言可能
-//  var val1 = "var変数を再宣言";
-//  console.log(val1);
+var val1 = "var変数を再宣言";
+console.log(val1);
 
 //  // 意図しない上書きや再宣言ができてしまう
 
-// let val2 = "let変数";
-// console.log(val2);
+let val2 = "let変数";
+console.log(val2);
 
 // // letは上書きが可能
-// val2 = "let変数を上書き";
-// console.log(val2);
+val2 = "let変数を上書き";
+console.log(val2);
 
 // // let再宣言不可能
 // let val2 = "let変数を再宣言";
 
-// const val3 = "const変数";
-// console.log(val3);
+const val3 = "const変数";
+console.log(val3);
 
 // // const変数は上書き不可能
 // val3 = "const変数を上書き";
@@ -34,41 +34,41 @@
 // // const変数は再宣言不可能
 // const val3 = "const変数を再宣言";
 
-// // constで定義したオブジェクトはプロパティの変更が可能
-// // オブジェクトはほとんどconst
-// const val4 = {
-//   name: 'ネロ',
-//   age: 10,
-// };
-// val4.name = "nero";
-// val4.address = "Nagoya";
-// console.log(val4);
+// constで定義したオブジェクトはプロパティの変更が可能
+// オブジェクトはほとんどconst
+const val4 = {
+  name: "ネロ",
+  age: 10
+};
+val4.name = "nero";
+val4.address = "Nagoya";
+console.log(val4);
 
 // constで定義した配列はプロパティの変更が可能
-// const val5 = ['dog', 'cat'];
-// val5[0] = "bird";
-// val5.push("monkey");
-// console.log(val5);
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
 
 /**
  * テンプレート文字列
  */
-//  const name = "ネロ";
-//  const age = 10;
-//  // 「私の名前はネロです。年齢は10歳です。」
+const name = "ネロ";
+const age = 10;
+// 「私の名前はネロです。年齢は10歳です。」
 
-//  // 従来の方法
-//  const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-//  console.log(message1);
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+console.log(message1);
 
-//  //　 テンプレート文字列を用いた方法
-//  const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-//  console.log(message2);
+//　 テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
 
 /**
  * アロー関数
  */
-// 従来の関数
+// // 従来の関数
 // function func1(str) {
 //   return str;
 // }
@@ -78,17 +78,17 @@
 // console.log(func1("func1です"));
 
 // アロー関数
-// const func2 = (str) => {
-//   return str;
-// }
+const func2 = (str) => {
+  return str;
+};
 // // 省略形
 // const func2 = str => str;
-// console.log(func2("func2です"));
+console.log(func2("func2です"));
 
-// const func3 = (num1, num2) => {
-//   return num1 + num2;
-// };
-// console.log(func3(10, 20));
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
 
 /**
  * 分割代入
@@ -117,121 +117,122 @@
 /**
  * デフォルト値、引数など
  */
-// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
-// sayHello();
-// sayHello("ネロ");
+const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+sayHello();
+sayHello("ネロ");
 
 /**
  * スプレッド構文 ...
  */
 // 配列の展開
-// const arr1 = [1, 2];
-// // console.log(arr1);
-// // console.log(...arr1);
+const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
 
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// // sumFunc(arr1[0], arr1[1]);
-// sumFunc(...arr1);
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
 
-// // まとめる
-// const arr2 = [1, 2, 3, 4, 5];
-// const [num1, num2, ...arr3] = arr2;
-// console.log(num1);
-// console.log(num2);
-// console.log(arr3);
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
 
-// // 配列のコピー、結合
-// const arr4 = [10, 20];
-// const arr5 = [30, 40];
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
 
-// const arr6 = [...arr4];
-// console.log(arr6);
-// arr6[0] = 1000;
-// console.log(arr6);
-// console.log(arr4);
+const arr6 = [...arr4];
+console.log(arr6);
+arr6[0] = 1000;
+console.log(arr6);
+console.log(arr4);
 
-// const arr7 = [...arr4, ...arr5];
-// console.log(arr7);
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
 
-// const arr8 = arr4;
-// console.log(arr8);
-// arr8[0] = 100;
-// console.log(arr8);
-// console.log(arr4);
-// // 元の配列に影響が出てしまうため、このようなコピーはしない
+const arr8 = arr4;
+console.log(arr8);
+arr8[0] = 100;
+console.log(arr8);
+console.log(arr4);
+// 元の配列に影響が出てしまうため、このようなコピーはしない
 
 /**
  * mapやfilterを使った配列の処理
  */
-// const nameArr = ["田中", "山田", "ネロ"];
-// // 従来
-// // for (let index = 0; index < nameArr.length; index++) {
-// //   // console.log(nameArr[index]);
-// //   console.log(`${index + 1}番目は${nameArr[index]}です`);
-// // }
+const nameArr = ["田中", "山田", "ネロ"];
+// 従来
+// for (let index = 0; index < nameArr.length; index++) {
+//   // console.log(nameArr[index]);
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
 
-// // // map
-// // const nameArr2 = nameArr.map((name)=>{
-// //   return name;
-// // })
-// // console.log(nameArr2);
+// // map
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
 
-// // nameArr.map((name) => console.log(name));
-// // nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+// nameArr.map((name) => console.log(name));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
 
-// // //filter ある条件に一致したものだけ返却して新しい配列を生成する
-// // const numArr = [1, 2, 3, 4, 5];
-// // const newNumArr = numArr.filter((num) => {
-// //   return num % 2 === 1;
-// // });
-// // console.log(newNumArr);
-
-// const newNameArr = nameArr.map((name) => {
-//   if (name === "ネロ") {
-//     return name;
-//   } else {
-//     return `${name}さん`;
-//   }
+// //filter ある条件に一致したものだけ返却して新しい配列を生成する
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
 // });
-// console.log(newNameArr);
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "ネロ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
 
 /**
  * 三項演算子
  */
 // ある条件 ? 条件がtrueの時　: 条件がfolseの時
-// const vall = 1 > 0 ? 'trueです' : 'falseです';
-// console.log(vall);
+const vall = 1 > 0 ? "trueです" : "falseです";
+console.log(vall);
 
-// const num = 1300;
-// // console.log(num.toLocaleString());
+const num = 1300;
+// console.log(num.toLocaleString());
 
-// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
-// console.log(formattedNum);
+const formattedNum =
+  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+console.log(formattedNum);
 
-// const checkSum = (num1, num2) => {
-//   return num1 + num2 > 100 ? '100を超えています！！'　: '許容範囲内です';
-// };
-// console.log(checkSum(50, 60));
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています！！" : "許容範囲内です";
+};
+console.log(checkSum(50, 60));
 
 /**
  * 論理演算子の本当の意味を知ろう　 && ||
  */
-// const flag1 = true;
-// const flag2 = false;
+const flag1 = true;
+const flag2 = false;
 
-// if (flag1 || flag2) {
-//   console.log("1か2はtrueになります");
-// }
-// if (flag1 && flag2) {
-//   console.log("1も2もtrueになります");
-// }
+if (flag1 || flag2) {
+  console.log("1か2はtrueになります");
+}
+if (flag1 && flag2) {
+  console.log("1も2もtrueになります");
+}
 
-// // || は左側がfalseなら右側を返す
-// const num = null;
-// const fee = num || "金額未設定です";
-// console.log(fee);
+// || は左側がfalseなら右側を返す
+const number1 = null;
+const fee = number1 || "金額未設定です";
+console.log(fee);
 
-// // && は左側がtrueなら右側を返す
-// const num2 = 100;
-// const fee2 = num2 && "何か設定されました";
-// console.log(fee2);
+// && は左側がtrueなら右側を返す
+const number2 = 100;
+const fee2 = number2 && "何か設定されました";
+console.log(fee2);
